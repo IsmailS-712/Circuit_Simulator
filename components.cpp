111e23e96a645fa12da74dde96f045df7069a363
thomm.o <olijdthompson@gmail.com>
2021-05-12 13:52:24 +0100

    Rewrite components into declarations and definitions

--- components.cpp
+++ components.cpp
@@ -4,223 +4,134 @@
 
 #include <vector>
 #include <string>
-#include <sstream>
 #include <iterator>
 
+#include "components.h"
 
-struct ParseReturn {
-    std::string identifier;
-    std::vector<std::string> nodes;
-    std::string value;
-};
+// Definition of Component members
+std::string Component::get_value() {
+    return this->value;
+}
 
+bool Component::represented_by(const std::string &token_str) {
+    return token_str.rfind(this->identifier, 0) == 0;
+}
 
-class Component {
-public:
-    std::string get_value() {
-        return this->value;
-    };
+ParseReturn* Component::parse(const std::string &token_str) {
+    auto* parsed = new ParseReturn;
 
-    bool represented_by(const std::string& token_str) {
-        return token_str.rfind(this->identifier, 0) == 0;
-    };
+    std::istringstream buf(token_str);
+    std::istream_iterator<std::string> beg(buf), end;
+    std::vector<std::string> tokens(beg, end);
 
-protected:
-    char identifier;
-    std::string value;
+    parsed->identifier = tokens[0];
+    parsed->value = tokens[tokens.size() - 1];
 
-    ParseReturn* parse(const std::string& token_str) {
-        auto* parsed = new ParseReturn;
-
-        std::istringstream buf(token_str);
-        std::istream_iterator<std::string> beg(buf), end;
-        std::vector<std::string> tokens(beg, end);
-
-        parsed->identifier = tokens[0];
-        parsed->value = tokens[tokens.size() - 1];
-
-        for (int i = 1; i < tokens.size(); i++) {
-            parsed->nodes.push_back(tokens[i]);
-        }
-        return parsed;
+    for (int i = 1; i < tokens.size(); i++) {
+        parsed->nodes.push_back(tokens[i]);
     }
-};
+    return parsed;
+}
 
 
-class IndependentVoltageSource: public Component {
-public:
-    IndependentVoltageSource(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of IndependentVoltageSource members
+IndependentVoltageSource::IndependentVoltageSource(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->positive_node = parsed->nodes[0];
-        this->negative_node = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'V';
-
-    std::string positive_node;
-    std::string negative_node;
-};
+    this->positive_node = parsed->nodes[0];
+    this->negative_node = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class CurrentSource: public Component {
-public:
-    CurrentSource(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of CurrentSource members
+CurrentSource::CurrentSource(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->input_node = parsed->nodes[0];
-        this->output_node = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'I';
-
-    std::string input_node;
-    std::string output_node;
-};
+    this->input_node = parsed->nodes[0];
+    this->output_node = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class Resistor: public Component {
-public:
-    Resistor(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of Resistor members
+Resistor::Resistor(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->input_node = parsed->nodes[0];
-        this->output_node = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'R';
-
-    std::string input_node;
-    std::string output_node;
-};
+    this->input_node = parsed->nodes[0];
+    this->output_node = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class Capacitor: public Component {
-public:
-    Capacitor(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of Capacitor members
+Capacitor::Capacitor(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->input_node = parsed->nodes[0];
-        this->output_node = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'C';
-
-    std::string input_node;
-    std::string output_node;
-};
+    this->input_node = parsed->nodes[0];
+    this->output_node = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class Inductor: public Component {
-public:
-    Inductor(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of Inductor members
+Inductor::Inductor(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->input_node = parsed->nodes[0];
-        this->output_node = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'L';
-
-    std::string input_node;
-    std::string output_node;
-};
+    this->input_node = parsed->nodes[0];
+    this->output_node = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class Diode: public Component {
-public:
-    Diode(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of Diode members
+Diode::Diode(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->anode = parsed->nodes[0];
-        this->cathode = parsed->nodes[1];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'I';
-
-    std::string anode;
-    std::string cathode;
-};
+    this->anode = parsed->nodes[0];
+    this->cathode = parsed->nodes[1];
+    delete parsed;
+}
 
 
-class BipolarJunctionTransistor: public Component {
-public:
-    BipolarJunctionTransistor(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of BipolarJunctionTransistor members
+BipolarJunctionTransistor::BipolarJunctionTransistor(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->collector = parsed->nodes[0];
-        this->base = parsed->nodes[1];
-        this->emitter = parsed->nodes[2];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'Q';
-
-    std::string collector;
-    std::string base;
-    std::string emitter;
-};
+    this->collector = parsed->nodes[0];
+    this->base = parsed->nodes[1];
+    this->emitter = parsed->nodes[2];
+    delete parsed;
+}
 
 
-class Mosfet: public Component {
-public:
-    Mosfet(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of Mosfet members
+Mosfet::Mosfet(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->drain = parsed->nodes[0];
-        this->gate = parsed->nodes[1];
-        this->source = parsed->nodes[2];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'M';
-
-    std::string drain;
-    std::string gate;
-    std::string source;
-};
+    this->drain = parsed->nodes[0];
+    this->gate = parsed->nodes[1];
+    this->source = parsed->nodes[2];
+    delete parsed;
+}
 
 
-class VoltageControlledCurrentSource: public Component {
-public:
-    VoltageControlledCurrentSource(const std::string& token_str) {
-        ParseReturn* parsed = this->parse(token_str);
-        this->value = parsed->value;
+// Definition of VoltageControlledCurrentSource members
+VoltageControlledCurrentSource::VoltageControlledCurrentSource(const std::string &token_str) {
+    ParseReturn* parsed = this->parse(token_str);
+    this->value = parsed->value;
 
-        this->positive = parsed->nodes[0];
-        this->negative = parsed->nodes[1];
-        this->control_positive = parsed->nodes[2];
-        this->control_negative = parsed->nodes[3];
-        delete parsed;
-    }
-
-protected:
-    char identifier = 'G';
-
-    std::string positive;
-    std::string negative;
-    std::string control_positive;
-    std::string control_negative;
-};
+    this->positive = parsed->nodes[0];
+    this->negative = parsed->nodes[1];
+    this->control_positive = parsed->nodes[2];
+    this->control_negative = parsed->nodes[3];
+    delete parsed;
+}
--- components.h
+++ components.h
@@ -2,3 +2,138 @@
 // Created by Oliver on 12/05/2021.
 //
 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+
+
+struct ParseReturn {
+    std::string identifier;
+    std::vector<std::string> nodes;
+    std::string value;
+};
+
+
+class Component {
+public:
+    std::string get_value();
+    bool represented_by(const std::string& token_str);
+
+protected:
+    char identifier;
+    std::string value;
+    ParseReturn* parse(const std::string& token_str);
+};
+
+
+class IndependentVoltageSource: public Component {
+public:
+    IndependentVoltageSource(const std::string& token_str);
+
+protected:
+    char identifier = 'V';
+    std::string value;
+    std::string positive_node;
+    std::string negative_node;
+};
+
+
+class CurrentSource: public Component {
+public:
+    CurrentSource(const std::string& token_str);
+
+protected:
+    char identifier = 'I';
+    std::string value;
+    std::string input_node;
+    std::string output_node;
+};
+
+
+class Resistor: public Component {
+public:
+    Resistor(const std::string& token_str);
+
+protected:
+    char identifier = 'R';
+    std::string value;
+    std::string input_node;
+    std::string output_node;
+};
+
+
+class Capacitor: public Component {
+public:
+    Capacitor(const std::string& token_str);
+
+protected:
+    char identifier = 'C';
+    std::string value;
+    std::string input_node;
+    std::string output_node;
+};
+
+
+class Inductor: public Component {
+public:
+    Inductor(const std::string& token_str);
+
+protected:
+    char identifier = 'L';
+    std::string value;
+    std::string input_node;
+    std::string output_node;
+};
+
+
+class Diode: public Component {
+public:
+    Diode(const std::string& token_str);
+
+protected:
+    char identifier = 'I';
+    std::string value;
+    std::string anode;
+    std::string cathode;
+};
+
+
+class BipolarJunctionTransistor: public Component {
+public:
+    BipolarJunctionTransistor(const std::string& token_str);
+
+protected:
+    char identifier = 'Q';
+    std::string value;
+    std::string collector;
+    std::string base;
+    std::string emitter;
+};
+
+
+class Mosfet: public Component {
+public:
+    Mosfet(const std::string& token_str);
+
+protected:
+    char identifier = 'M';
+    std::string value;
+    std::string drain;
+    std::string gate;
+    std::string source;
+};
+
+
+class VoltageControlledCurrentSource: public Component {
+public:
+    VoltageControlledCurrentSource(const std::string& token_str);
+
+protected:
+    char identifier = 'G';
+    std::string value;
+    std::string positive;
+    std::string negative;
+    std::string control_positive;
+    std::string control_negative;
+};
