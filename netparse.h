1f8c96740e45f9a6440d5d5fd3250da98cafa7e6
thomm.o <olijdthompson@gmail.com>
2021-06-05 15:10:44 +0100

    minor changes

--- netparse.h
+++ netparse.h
@@ -12,7 +12,6 @@
 
 
 std::vector<std::string>* load_netlist(const std::string& filename);
-
 void parse_netlist_to_components(std::vector<Component*>& components, std::vector<std::string>* netlist);
 
 #endif
